a45694e780f64fbd5dd2452839c2662a7885a381
Zeyu Li <li.zeyu@northeastern.edu>
2023-09-22 20:32:30 -0400

    Some progress about project0

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author Zeyu Li
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -137,7 +137,13 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++){
+            for (int j = 0; j < b.size(); j++){
+                if (b.tile(i,j) == null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -147,7 +153,15 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++){
+            for (int j = 0; j < b.size(); j ++) {
+                if (b.tile(i,j) != null){
+                    if (b.tile(i,j).value() == MAX_PIECE){
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
@@ -158,7 +172,22 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        if (emptySpaceExists(b)){
+            return true;
+        }else {
+            for (int i = 0; i < (b.size()-1);i++){
+                for (int j = 0; j < (b.size() - 1); j ++){
+                    if ((b.tile(i,j).value() == b.tile((i+1),j).value()) || (b.tile(i,j).value() == b.tile(i,(j+1)).value())){
+                        return true;
+                    }
+                }
+            }
+        }
+        int corner = b.size() - 1;
+        if ((b.tile(corner,corner).value() == b.tile((corner-1),corner).value()) || (b.tile(corner,corner).value() == b.tile(corner,(corner-1)).value())){
+            return true;
+        }
+
         return false;
     }
 
